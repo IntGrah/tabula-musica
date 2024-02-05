c1fe7c677f0dcbd5f5c68e28a2e0d3a3a9735e50
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-02-05 01:03:23 +0000

    Refactor

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,11 +1,17 @@
-import Footer from "../ui/footer";
-import Events from "./events";
+import WRCHCUO from "./24-02-10-wrch-cuo";
+import GirtonLowe from "./24-02-25-girton-lowe";
+import TrinityOboe from "./24-02-24-trinity-oboe";
+import ClareOpera from "./24-03-09-clare-opera";
+import TrinityTCO from "./24-03-09-trinity-tco";
 
-export default function EventsPage() {
+export default function Events() {
   return (
-    <div>
-      <Events />
-      <div className="h-96"></div>
+    <div className="flex flex-wrap bg-gray-800 text-white shadow-2xl transition-all">
+      <WRCHCUO />
+      <TrinityOboe />
+      <GirtonLowe />
+      <ClareOpera />
+      <TrinityTCO />
     </div>
   );
 }

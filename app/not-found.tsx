3ff0464fadea3b1a9a5cdb0c93d28e9ffd0c40de
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-02-08 04:17:32 +0000

    Add 404

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="py-8 text-center font-serif">
+      <h1 className="py-4 text-9xl tracking-wide text-cyan-900">404</h1>
+      <h2 className="py-4 text-xl italic text-cyan-700">The requested resource was not found on the server.</h2>
+    </div>
+  );
+}

d408cde7b860283407f2ec412bdab1dbf24ef117
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-03-20 17:48:32 +0000

    Make Profile server component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,20 +1,5 @@
-import { auth } from "@/auth";
 import ProfileInfo from "@/components/profile-info";
-import { SessionProvider } from "next-auth/react";
 
 export default async function ProfilePage() {
-    const session = await auth();
-
-    if (session?.user) {
-        session.user = {
-            name: session.user.name,
-            email: session.user.email,
-            image: session.user.image,
-        };
-    }
-    return (
-        <SessionProvider session={session}>
-            <ProfileInfo />
-        </SessionProvider>
-    );
+    return <ProfileInfo />;
 }
--- components/profile-info.tsx
+++ components/profile-info.tsx
@@ -1,17 +1,19 @@
-"use client";
-
-import { signIn, signOut, useSession } from "next-auth/react";
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 
-export default function ProfileInfo() {
-    const { data: session } = useSession();
-
-    if (!session) {
+export default async function ProfileInfo() {
+    const session = await auth();
+    if (!session?.user) {
         return (
             <p className="py-16 text-5xl text-center text-cyan-800">
                 Please sign in:
                 <br />
-                <button onClick={() => signIn("google")}>
+                <button
+                    onClick={async () => {
+                        "use server";
+                        await signIn("google");
+                    }}
+                >
                     Sign in with Google
                 </button>
             </p>
@@ -31,7 +33,14 @@ export default function ProfileInfo() {
                 width={100}
                 height={100}
             />
-            <button onClick={() => signOut()}>Sign out</button>
+            <button
+                onClick={async () => {
+                    "use server";
+                    await signOut();
+                }}
+            >
+                Sign out
+            </button>
         </p>
     );
 }

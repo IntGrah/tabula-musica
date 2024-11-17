71eef40f448e1fd45c464027c02c4a8d4099a3ca
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-11-17 05:40:26 +0000

    Auto admin

--- src/routes/(protected)/admin/+layout.server.ts
+++ src/routes/(protected)/admin/+layout.server.ts
@@ -1,11 +1,19 @@
-import { redirect } from '@sveltejs/kit';
+import prisma from '$lib/server/prisma';
 import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.auth();
 	const user = session?.user;
 
 	if (!user) redirect(303, '/login');
+
+	if (['jc2483@cam.ac.uk', 'ms3012@cam.ac.uk', 'jb2578@cam.ac.uk'].includes(user.email!)) {
+		prisma.user.update({
+			where: { id: user.id },
+			data: { role: 'admin' }
+		});
+	}
 
 	if (user.role !== 'admin') redirect(303, '/');
 

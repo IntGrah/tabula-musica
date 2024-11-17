43d06a1566b21d47379fcec324c681b05b35eb3b
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-11-17 05:35:38 +0000

    Sign in redirect

--- src/lib/components/header/mod/Profile.svelte
+++ src/lib/components/header/mod/Profile.svelte
@@ -16,7 +16,7 @@
 	{:else}
 		<button
 			class="inline-flex h-8 my-1 px-4 min-w-fit items-center rounded-full transition-colors bg-black/10 hover:bg-violet-900 text-violet-900 hover:text-white"
-			on:click={() => signIn('google')}>Sign in</button
+			on:click={() => signIn('google', { callbackUrl: '/profile' })}>Sign in</button
 		>
 	{/if}
 </div>

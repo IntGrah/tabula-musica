20defcf0ee1a2fd6aec087ee24d540c5686aafa9
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-11-17 04:58:31 +0000

    return session

--- src/routes/(protected)/profile/+page.server.ts
+++ src/routes/(protected)/profile/+page.server.ts
@@ -1,5 +1,9 @@
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ parent }) => {
+export const load: PageServerLoad = async ({ parent, locals }) => {
 	await parent();
+
+	const session = await locals.auth();
+
+	return { session };
 };

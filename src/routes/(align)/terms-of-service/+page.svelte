677c51353a6c589cd954ce1242fc8cd80ac9b546
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-11-17 19:23:15 +0000

    ToS

---
+++ src/routes/(align)/terms-of-service/+page.svelte
@@ -0,0 +1,7 @@
+<h2>Terms of service</h2>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit deleniti placeat nemo,
+	exercitationem asperiores assumenda quam quos recusandae vitae, eius tenetur, mollitia voluptatum
+	similique distinctio! Expedita modi neque aperiam totam.
+</p>

6f42cf779c09f921ca3248ee5095f78922307164
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-11-17 04:31:19 +0000

    prisma hack

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient as ImportedPrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
+
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
+
+class PrismaClient extends _PrismaClient {}
 
 const prisma = new PrismaClient();
 export const adapter = PrismaAdapter(prisma);
